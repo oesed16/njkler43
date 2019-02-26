fdb83896d2091248f460008a7714a83f040a67b1
Angela Castagno <oesed16@gmail.com>
2019-02-26 12:36:15 -0500

    Reto 12 React: Filtrando Posts

--- src/App.js
+++ src/App.js
@@ -5,17 +5,39 @@ import posts from './posts'
 // Modifica el componente App para implmentar la funcionalidad requerida
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      search: ''
+    };
+  }
+
+  updateSearch(e){
+    this.setState({
+      search: e.target.value.substr(0, 50)
+    });
+  }
+
   render() {
+    let filteredPosts = posts.filter(
+      (post) => {
+        return post.title.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1;
+      }
+    )
     return (
       <div>
         <div className="filter">
-          <input type="text" placeholder="Ingresa el término de búsqueda" />
+          <input type="text" value={this.state.search} 
+          onChange={this.updateSearch.bind(this)} 
+          placeholder="Ingresa el término de búsqueda" />
         </div>
         <ul>
-          <li>
-            <a href={posts[0].url}><img src={posts[0].image } /></a>
-            <p>{ posts[0].title }</p>
+          {filteredPosts.map((post, index) =>
+          <li key={index}>
+            <a href={post.url}><img src={post.image } alt="PhotoLink" /></a>
+            <p>{ post.title }</p>
           </li>
+          )}
         </ul>
       </div>
     )
